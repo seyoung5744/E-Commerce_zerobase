9bc8b58329329a871fc9bbf3c238d80b1e964092
seyoung5744 <seyoung574458@gmail.com>
2023-01-27 18:16:47 +0900

    feat : [006-LOGIN-SELLER] 셀러에 대한 로그인 구현
    Changes
    ---
    
    1. Login을 통한 토큰 발행
    2. /seller/* 에 대한 Token Filter 적용

--- user-api/src/main/java/com/zerobase/cms/user/application/SignInApplication.java
+++ user-api/src/main/java/com/zerobase/cms/user/application/SignInApplication.java
@@ -4,8 +4,10 @@ import static com.zerobase.cms.user.exception.ErrorCode.LOGIN_CHECK_FAIL;
 
 import com.zerobase.cms.user.domain.SignInForm;
 import com.zerobase.cms.user.domain.model.Customer;
+import com.zerobase.cms.user.domain.model.Seller;
 import com.zerobase.cms.user.exception.CustomException;
 import com.zerobase.cms.user.service.customer.CustomerService;
+import com.zerobase.cms.user.service.seller.SellerService;
 import com.zerobase.domain.config.JwtAuthenticationProvider;
 import com.zerobase.domain.domain.common.UserType;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +18,8 @@ import org.springframework.stereotype.Service;
 public class SignInApplication {
 
     private final CustomerService customerService;
+    private final SellerService sellerService;
+
     private final JwtAuthenticationProvider provider;
 
 
@@ -27,5 +31,15 @@ public class SignInApplication {
         // 2. 토큰을 발행.
         // 3. 토큰을 response.
         return provider.createToken(c.getEmail(), c.getId(), UserType.CUSTOMER);
+    }
+
+    public String sellerLoginToken(SignInForm form) {
+        // 1. 로그인 가능 여부.
+        Seller s = sellerService.findValidSeller(form.getEmail(), form.getPassword())
+            .orElseThrow(() -> new CustomException(LOGIN_CHECK_FAIL));
+
+        // 2. 토큰을 발행.
+        // 3. 토큰을 response.
+        return provider.createToken(s.getEmail(), s.getId(), UserType.SELLER);
     }
 }
---
+++ user-api/src/main/java/com/zerobase/cms/user/config/filter/SellerFilter.java
@@ -0,0 +1,41 @@
+package com.zerobase.cms.user.config.filter;
+
+import com.zerobase.cms.user.service.customer.CustomerService;
+import com.zerobase.cms.user.service.seller.SellerService;
+import com.zerobase.domain.config.JwtAuthenticationProvider;
+import com.zerobase.domain.domain.common.UserVo;
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+
+@WebFilter(urlPatterns = "/seller/*") // customer로 들어가는 모든 요청에 대한 필터
+@RequiredArgsConstructor
+public class SellerFilter implements Filter {
+    // 토큰은 http 프로토콜에서 header에 포함됨. 이때 어떤 key를 기준으로 토큰을 주고 받을지에 대한 key값
+    public static final String TOKEN_HEADER = "X-AUTH-TOKEN";
+
+    private final JwtAuthenticationProvider jwtAuthenticationProvider;
+    private final SellerService sellerService;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        String token = req.getHeader(TOKEN_HEADER);
+
+        if(!jwtAuthenticationProvider.isValidateToken(token)){
+            throw new ServletException("Invalid Access");
+        }
+
+        UserVo vo = jwtAuthenticationProvider.getUserVo(token);
+        sellerService.findByIdAndEmail(vo.getId(), vo.getEmail())
+            .orElseThrow(() -> new ServletException("Invalid Access"));
+
+        chain.doFilter(request, response);
+    }
+}
