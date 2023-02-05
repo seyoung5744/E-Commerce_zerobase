b7f4d7e7f3421c26253660ec6a104e176334bcda
seyoung5744 <seyoung574458@gmail.com>
2023-02-05 15:00:51 +0900

    feat : [010-PRODUCT-ADD] Product Add, ProductItem Add
    추가로 Exception에 대한 Response의 visuality 향상

---
+++ order-api/src/main/java/com/zerobase/cms/order/domain/product/UpdateProductItemForm.java
@@ -0,0 +1,9 @@
+package com.zerobase.cms.order.domain.product;
+
+public class UpdateProductItemForm {
+    private Long id;
+    private Long productId;
+    private String name;
+    private Integer price;
+    private Integer count;
+}
