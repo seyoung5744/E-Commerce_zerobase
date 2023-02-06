34ead081a719db3d1311584c89f27d9f5b4687e4
seyoung5744 <seyoung574458@gmail.com>
2023-02-06 19:07:45 +0900

    feat : [017-PAYMENT] 장바구니 상품 결제

--- order-api/src/main/java/com/zerobase/cms/order/application/CartApplication.java
+++ order-api/src/main/java/com/zerobase/cms/order/application/CartApplication.java
@@ -34,7 +34,7 @@ public class CartApplication {
 
         Cart cart = cartService.getCart(customerId);
 
-        if (cart != null && !addAble(cart, product, form)) {
+        if (!addAble(cart, product, form)) {
             throw new CustomException(ITEM_COUNT_NOT_ENOUGH);
         }
         return cartService.addCart(customerId, form);
@@ -58,6 +58,8 @@ public class CartApplication {
     // 2. 상품의 가격이나 수량이 변동 된다.
     public Cart getCart(Long customerId) {
         Cart cart = refreshCart(cartService.getCart(customerId));
+        cartService.putCart(cart.getCustomerId(), cart);
+
         Cart returnCart = new Cart();
         returnCart.setCustomerId(customerId);
         returnCart.setProducts(cart.getProducts());
@@ -75,7 +77,7 @@ public class CartApplication {
         cartService.putCart(customerId, null);
     }
 
-    private Cart refreshCart(Cart cart) {
+    protected Cart refreshCart(Cart cart) {
         // 1. 상품이나 상품의 아이템의 정보, 가격, 수량이 변경되었는지 체크하고
         // 그에 맞는 알람 제공
 
@@ -162,7 +164,7 @@ public class CartApplication {
                 cart.addMessage(builder.toString());
             }
         }
-        cartService.putCart(cart.getCustomerId(), cart);
+
         return cart;
     }
 
